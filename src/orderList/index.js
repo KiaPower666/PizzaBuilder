e10fe7db72696d9e15add103aa5799a90783836c
Vladislav Asachuk <oxxxymiron6473@mail.ru>
2023-06-24 20:38:26 +0300

    task PPJ-5

--- src/orderList/index.js
+++ src/orderList/index.js
@@ -5,3 +5,34 @@ addClassFoundById("wrapper-main", styles.wrapper);
 addClassFoundById("wrapper-main", styles.container);
 addClassFoundById("viewingarea", styles.viewingarea);
 addClassFoundById("price-block", styles.price);
+
+const ingredientsDiv = document.getElementById('ingredients');
+const orderList = document.getElementById('order-list');
+
+ingredientsDiv.ingredients
+addEventListener('click', (e) => {
+  
+const inputs = document.querySelectorAll('.input1');
+  const inputsChecked = Array.from(inputs).filter((input) => input.checked);
+  
+ 
+updateOrderList(inputsChecked);
+});
+
+function updateOrderList(inputsChecked) {
+  orderList.innerHTML = ""; 
+  inputsChecked.forEach((input) => {
+    
+   
+const labelText = input.nextElementSibling.textContent;
+    
+   
+const ingredientItem = document.createElement('li');
+    ingredientItem.
+   
+textContent = labelText;
+    orderList.
+   
+appendChild(ingredientItem);
+  });
+}
