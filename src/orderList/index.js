516857618fb818242f6557b9dc3271830eb3481a
Vladislav Asachuk <oxxxymiron6473@mail.ru>
2023-06-25 22:55:33 +0300

    PPJ-4 add logic price

--- src/orderList/index.js
+++ src/orderList/index.js
@@ -1,5 +1,6 @@
 import styles from "./orderList.module.scss";
 import { addClassFoundById } from "../utils/addClassFoundById";
+import { ingredients } from "../orderList/ingredients";
 
 addClassFoundById("wrapper-main", styles.wrapper);
 addClassFoundById("wrapper-main", styles.container);
@@ -13,6 +14,7 @@ const inputs = document.querySelectorAll(".input1");
 inputsDiv.addEventListener("click", () => {
   const inputsChecked = Array.from(inputs).filter((input) => input.checked);
   updateOrderList(inputsChecked);
+  price();
 });
 
 function updateOrderList(inputsChecked) {
@@ -38,3 +40,20 @@ orderList.addEventListener("click", (event) => {
     event.target.remove();
   }
 });
+function price() { 
+  const totalPriceElement = document.getElementById("price");
+    let totalPrice = 0;
+  
+    inputs.forEach((input) => {
+      if (input.checked) {
+        const labelText = input.nextElementSibling.textContent;
+        Object.values(ingredients).forEach((category) => {
+          const ingredient = category.find((item) => item.name === labelText);
+          if (ingredient) {
+            totalPrice += ingredient.price;
+          }
+        });
+      }
+    });
+    totalPriceElement.textContent = totalPrice + " р.";
+  }
---
+++ src/orderList/ingredients.js
@@ -0,0 +1,28 @@
+export const ingredients = {
+    dough: [
+      { name: "Классика", price: 3 },
+      { name: "Тонкое", price: 4 },
+      { name: "Сырный борт", price: 6 },
+      { name: "Хот-Дог борт", price: 7 },
+    ],
+    meat: [
+      { name: "Бекон", price: 6 },
+      { name: "Ветчина", price: 7 },
+      { name: "Пепперони", price: 5 },
+      { name: "Баварские колбаски", price: 5 },
+    ],
+    vegetables: [
+      { name: "Сладкий перец", price: 2 },
+      { name: "Шампиньоны", price: 4 },
+      { name: "Халапеньо", price: 3 },
+      { name: "Томаты", price: 2 },
+    ],
+  
+    sauce: [
+      { name: "Барбекю", price: 2 },
+      { name: "Бургер", price: 1 },
+      { name: "Ранч", price: 1 },
+      { name: "Карри", price: 2 },
+    ],
+  };
+  
