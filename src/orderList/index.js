7d8b6f8d4b3691b646951bbc8047e157526db598
Vladislav Asachuk <oxxxymiron6473@mail.ru>
2023-06-25 13:36:09 +0300

    task PPJ-4

--- src/orderList/index.js
+++ src/orderList/index.js
@@ -7,11 +7,11 @@ addClassFoundById("viewingarea", styles.viewingarea);
 addClassFoundById("price-block", styles.price);
 
 const orderList = document.getElementById("order-list");
+const inputsDiv = document.getElementById("ingredients");
+const inputs = document.querySelectorAll(".input1");
 
-addEventListener("click", () => {
-  const inputs = document.querySelectorAll(".input1");
+inputsDiv.addEventListener("click", () => {
   const inputsChecked = Array.from(inputs).filter((input) => input.checked);
-
   updateOrderList(inputsChecked);
 });
 
@@ -19,9 +19,22 @@ function updateOrderList(inputsChecked) {
   orderList.innerHTML = "";
   inputsChecked.forEach((input) => {
     const labelText = input.nextElementSibling.textContent;
-
     const ingredientItem = document.createElement("li");
     ingredientItem.textContent = labelText;
     orderList.appendChild(ingredientItem);
   });
 }
+
+orderList.addEventListener("click", (event) => {
+  if (event.target.tagName === "LI") {
+    const ingredientText = event.target.textContent;
+    const currentInput = Array.from(inputs).find((input) => {
+      const labelText = input.nextElementSibling.textContent;
+      return labelText === ingredientText;
+    });
+    if (currentInput) {
+      currentInput.checked = false;
+    }
+    event.target.remove();
+  }
+});
