1a508e0d6fb6acf30bc0b76508c5a1095c172336
Elena.Krayeva <alenakrayeva@gmail.com>
2023-06-26 17:19:55 +0300

    add/remove to pizzaObj from igredients

--- src/ingredients/doughHandler.js
+++ src/ingredients/doughHandler.js
@@ -1,9 +1,13 @@
 import { sectionHandler } from "./sectionHandler";
 import { showPizzaSlice } from "../order/showPizzaslice";
+import { addToPizzaObj } from "../pizzaObject";
+import { pizzaObj } from "../pizzaObject";
 
 export const inputsChecked = [];
 
 export function doughHandler(e) {
   sectionHandler(e, inputsChecked, 1);
   showPizzaSlice ("dough", "square1"); 
+  addToPizzaObj(e, "dough", 1)
+  console.log(pizzaObj)
 }
